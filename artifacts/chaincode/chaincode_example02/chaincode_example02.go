907c13fee007f7df5e03cce5b4eb1c7559da838f
PaperHS <paperhs901230@gmail.com>
2018-11-12 10:56:48 +0800

    write a chaincode

--- artifacts/chaincode/chaincode_example02/chaincode_example02.go
+++ artifacts/chaincode/chaincode_example02/chaincode_example02.go
@@ -14,15 +14,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package main
-
 //WARNING - this chaincode's ID is hard-coded in chaincode_example04 to illustrate one way of
 //calling chaincode from a chaincode. If this example is modified, chaincode_example04.go has
 //to be modified as well with the new ID of chaincode_example02.
 //chaincode_example05 show's how chaincode ID can be passed in as a parameter instead of
 //hard-coding.
+package main
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 
@@ -30,170 +30,154 @@ import (
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
-// SimpleChaincode example simple Chaincode implementation
-type SimpleChaincode struct {
+type GameChainCode struct {
 }
 
-func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("ex02 Init")
+func (cc *GameChainCode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+
+	var sum string // Sum of asset holdings across accounts. Initially 0
+	var sumVal int // Sum of holdings
+	var err error
 	_, args := stub.GetFunctionAndParameters()
-	var A, B string    // Entities
-	var Aval, Bval int // Asset holdings
-	var err error
-
-	if len(args) != 4 {
-		return shim.Error("Incorrect number of arguments. Expecting 4")
-	}
-
-	// Initialize the chaincode
-	A = args[0]
-	Aval, err = strconv.Atoi(args[1])
-	if err != nil {
-		return shim.Error("Expecting integer value for asset holding")
-	}
-	B = args[2]
-	Bval, err = strconv.Atoi(args[3])
-	if err != nil {
-		return shim.Error("Expecting integer value for asset holding")
-	}
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-
-	// Write the state to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	return shim.Success(nil)
-}
-
-func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Println("ex02 Invoke")
-	function, args := stub.GetFunctionAndParameters()
-	if function == "invoke" {
-		// Make payment of X units from A to B
-		return t.invoke(stub, args)
-	} else if function == "delete" {
-		// Deletes an entity from its state
-		return t.delete(stub, args)
-	} else if function == "query" {
-		// the old "Query" is now implemtned in invoke
-		return t.query(stub, args)
-	}
-
-	return shim.Error("Invalid invoke function name. Expecting \"invoke\" \"delete\" \"query\"")
-}
-
-// Transaction makes payment of X units from A to B
-func (t *SimpleChaincode) invoke(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var A, B string    // Entities
-	var Aval, Bval int // Asset holdings
-	var X int          // Transaction value
-	var err error
-
-	if len(args) != 3 {
-		return shim.Error("Incorrect number of arguments. Expecting 3")
-	}
-
-	A = args[0]
-	B = args[1]
-
-	// Get the state from the ledger
-	// TODO: will be nice to have a GetAllState call to ledger
-	Avalbytes, err := stub.GetState(A)
-	if err != nil {
-		return shim.Error("Failed to get state")
-	}
-	if Avalbytes == nil {
-		return shim.Error("Entity not found")
-	}
-	Aval, _ = strconv.Atoi(string(Avalbytes))
-
-	Bvalbytes, err := stub.GetState(B)
-	if err != nil {
-		return shim.Error("Failed to get state")
-	}
-	if Bvalbytes == nil {
-		return shim.Error("Entity not found")
-	}
-	Bval, _ = strconv.Atoi(string(Bvalbytes))
-
-	// Perform the execution
-	X, err = strconv.Atoi(args[2])
-	if err != nil {
-		return shim.Error("Invalid transaction amount, expecting a integer value")
-	}
-	Aval = Aval - X
-	Bval = Bval + X
-	fmt.Printf("Aval = %d, Bval = %d\n", Aval, Bval)
-
-	// Write the state back to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	return shim.Success(nil)
-}
-
-// Deletes an entity from state
-func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	if len(args) != 1 {
 		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
-
-	A := args[0]
-
-	// Delete the key from the state in ledger
-	err := stub.DelState(A)
+	// Initialize the chaincode
+	sum = args[0]
+	sumVal, err = strconv.Atoi(sum)
 	if err != nil {
-		return shim.Error("Failed to delete state")
+		return shim.Error("Expecting integer value for sum")
 	}
-
+	fmt.Printf("sumVal = %d\n", sumVal)
+	if sumVal < 100 {
+		return shim.Error("init value must bigger than 100")
+	}
+	// Write the state to the ledger
+	err = stub.PutState("total", []byte(strconv.Itoa(sumVal)))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
 	return shim.Success(nil)
 }
 
-// query callback representing the query of a chaincode
-func (t *SimpleChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var A string // Entities
-	var err error
+func (cc *GameChainCode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	function, args := stub.GetFunctionAndParameters()
+	if function == "query" {
+		return cc.query(stub, args)
+	} else if function == "lottery" {
+		return cc.lottery(stub, args)
+	} else if function == "create_user" {
+		return cc.create_user(stub, args)
+	}
+	return shim.Error("Invalid invoke function name. Expecting query uid,lottery uid token,create_user uid")
+}
 
+func (cc *GameChainCode) create_user(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	if len(args) != 1 {
-		return shim.Error("Incorrect number of arguments. Expecting name of the person to query")
+		return shim.Error("params error")
+	}
+	uId := args[0]
+	balance, err := cc.getBalance(stub, uId)
+	if balance < 0 || err != nil {
+		stub.PutState(uId, []byte("1000"))
+		return shim.Success([]byte("1000"))
+	} else {
+		return shim.Error("uid has been registed")
+	}
+}
+
+func (cc *GameChainCode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) == 0 {
+		t, err := cc.getTotal(stub)
+		if err != nil {
+			return shim.Error("get total error!")
+		}
+		if t < 100 {
+			return shim.Success([]byte("false"))
+		} else {
+			return shim.Success([]byte("true"))
+		}
+	} else {
+		t, err := cc.getBalance(stub, args[0])
+		if err != nil {
+			return shim.Error("get balance error!")
+		} else {
+			return shim.Success([]byte(strconv.Itoa(t)))
+		}
 	}
 
-	A = args[0]
+}
 
-	// Get the state from the ledger
-	Avalbytes, err := stub.GetState(A)
+func (cc *GameChainCode) lottery(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 2 {
+		return shim.Error("params error")
+	}
+	t, err := cc.getTotal(stub)
 	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
-		return shim.Error(jsonResp)
+		return shim.Error("get total error!")
+	}
+	uId := args[0]
+	uWager, err := strconv.Atoi(args[1])
+	if err != nil {
+		return shim.Error("wager error!")
+	}
+	err = cc.plus(stub, uId, -uWager)
+	if err != nil {
+		return shim.Error("balance not enough")
 	}
 
-	if Avalbytes == nil {
-		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
-		return shim.Error(jsonResp)
+	if (uWager - t) == 0 {
+		//hit
+		err = cc.plus(stub, uId, uWager+t)
+		return shim.Success([]byte("you win! game over!"))
+	} else if ((t - uWager) > t>>2) && ((t - uWager) < t>>2+t>>3) {
+		err = cc.plus(stub, uId, uWager>>1)
+		return shim.Success([]byte("congratulation!!!!"))
+	} else {
+		err = cc.plus(stub, "total", uWager)
+		return shim.Success([]byte("Good Luck Next Time!"))
 	}
+	return shim.Error("bet error")
 
-	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
-	fmt.Printf("Query Response:%s\n", jsonResp)
-	return shim.Success(Avalbytes)
+}
+
+//plus or minus value of key
+func (cc *GameChainCode) plus(stub shim.ChaincodeStubInterface, k string, v int) error {
+	Avalbytes, err := stub.GetState(k)
+	if err != nil {
+		return err
+	}
+	TotalV, err := strconv.Atoi(string(Avalbytes))
+	if err != nil {
+		return err
+	}
+	TotalV += v
+	if TotalV < 0 {
+		return errors.New("balance not enough")
+	}
+	stub.PutState(k, []byte(strconv.Itoa(TotalV)))
+	return nil
+}
+
+func (cc *GameChainCode) getBalance(stub shim.ChaincodeStubInterface, k string) (int, error) {
+	Avalbytes, err := stub.GetState(k)
+	if err != nil {
+		return -1, err
+	}
+	TotalV, err := strconv.Atoi(string(Avalbytes))
+	if err != nil {
+		return -1, err
+	}
+	return TotalV, nil
+}
+
+func (cc *GameChainCode) getTotal(stub shim.ChaincodeStubInterface) (int, error) {
+	return cc.getBalance(stub, "total")
 }
 
 func main() {
-	err := shim.Start(new(SimpleChaincode))
+	err := shim.Start(new(GameChainCode))
 	if err != nil {
-		fmt.Printf("Error starting Simple chaincode: %s", err)
+		fmt.Printf("Error starting game chaincode: %s", err)
 	}
 }
